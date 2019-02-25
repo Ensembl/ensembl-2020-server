711a40e58fa42f45a1cd53e9d10dccadbff2c69f
Dan Sheppard <dan@ebi.ac.uk>
2019-02-25 02:43:04 +0000

    Update of test server with contig data.

--- demo/flask/server.py
+++ demo/flask/server.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 import pyBigWig
 
@@ -11,25 +13,31 @@ contig_path = "/home/dan/e2020_march_datafiles/contigs/contigs-approx.bb"
 def get_contig_data(chromosome,start,end):
     bb = pyBigWig.open(contig_path)
     out = bb.entries(chromosome,start,end) or []
-    print(out)
     bb.close()
     return out
 
 def burst_leaf(leaf):
-    (chrom,rest) = leaf.rsplit(':',2)
-    (start,end) = rest.split('-')
+    (chrom,rest) = leaf.rsplit(':',1)
+    (start,end) = rest.split('-',1)
+    chrom = "6" # XXX
     return (chrom,int(start),int(end))
 
 @app.route("/browser/data/contig-normal/<leaf>")
 def contig_normal(leaf):
-    (chrom,start,end) = burst_leaf(leaf)
-    data = get_contig_data(chrom,start,end)
     starts = []
     lens = []
-    for (start,end,_) in data:
-        starts.append(start)
-        lens.append(end-start)
-    data = {'data':[starts,lens]}
+    senses = []
+    try:
+        (chrom,start,end) = burst_leaf(leaf)
+        data = get_contig_data(chrom,start,end)
+        for (start,end,extra) in data:
+            extra = extra.split("\t")
+            starts.append(start)
+            lens.append(end-start)
+            senses.append(True if extra[2]=='+' else False)
+    except ValueError:
+        pass
+    data = {'data':[starts,lens,senses]}
     return jsonify(data)
   
 if __name__ == "__main__":
